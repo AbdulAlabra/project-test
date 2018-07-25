f5c9736fd926a38dfc8fbaa6daea7b08b10963b2
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-07-25 14:00:31 -0700

    we have to show 2 charts one for stock & one for crypto

--- google-api.js
+++ google-api.js
@@ -3,6 +3,8 @@ var crypto_chosen_name;
 var stock_chosen_date_1;
 var stock_chosen_date_2;
 var money_invested;
+var choseStock = false;
+var choseCrypto = false;
 
 $('button').on('click', function () {
     stock_chosen_name = $('.stock-search').val();
@@ -18,21 +20,28 @@ $('button').on('click', function () {
     if (stock_chosen_name.length >= 3 && stock_chosen_name.length < 5) {
         processURL(queryURL);
         debugger;
+        choseStock = true;
     }
     else {
+
         alert('I did not run: Stock');
     }
 
     if (crypto_chosen_name.length >= 3 && crypto_chosen_name.length < 5) {
-
-        processURL(crypto_queryURL);
-        debugger;
-
+        choseCrypto = true;
+         processURL(crypto_queryURL);
     }
     else {
-    alert('I did not run: BTC');
+        alert('I did not run: BTC');
     }
 }); //end of the click button 
+
+
+
+
+
+
+
 
 
 function processURL(queryURL) {
@@ -43,6 +52,7 @@ function processURL(queryURL) {
     })
         .then(function (response) {
 
+            
             var turn_obj_to_array = Object.values(response);
             var stock_crypto_info = turn_obj_to_array[0];
 
@@ -50,7 +60,7 @@ function processURL(queryURL) {
                 //this changes one of the key names so that the rest of the code run in the same way for both crypto and stock.
                 stock_crypto_info['3. Last Refreshed'] = stock_crypto_info['6. Last Refreshed']
                 delete stock_crypto_info['6. Last Refreshed']
-            }  
+            }
 
             var stock_crypto_price = Object.values(turn_obj_to_array[1]);
             var stock_crypto_dates = Object.keys(turn_obj_to_array[1]);
@@ -60,7 +70,7 @@ function processURL(queryURL) {
                 var date_index = stock_crypto_dates.indexOf(x);
                 var price = stock_crypto_price[date_index];
                 price.date = x
-                
+
                 //this will change some of the key names for crypto objects so that the rest of the code can function in the same way as stock
                 if (price['2. high'] === undefined) {
                     var high_crypto_val = price['2b. high (USD)'];
@@ -90,10 +100,9 @@ function processURL(queryURL) {
                     return price
                 }
             }); // end of the new array
-            
-            console.log(stock_crypto_info);
-                        debugger;
 
+            console.log(newArray);
+            debugger;
             //finds the highest price for stock/crypto in whatever time frame you choose.   
             var stock_crypto_highest_price = newArray.reduce(function (x, obj) {
                 var y = obj['2. high'];
@@ -104,8 +113,8 @@ function processURL(queryURL) {
                     return Number(x)
                 }
             }, 0);
+            
             console.log(stock_crypto_highest_price);
-
             //finds the lowest price for stock/crypto in whatever time frame you choose.   
             var stock_crypto_lowest = newArray.reduce(function (x, obj) {
                 var y = obj['3. low'];
@@ -121,12 +130,12 @@ function processURL(queryURL) {
             var highest_price_info = newArray.find(function (obj) {
                 return obj['2. high'] === stock_crypto_highest_price.toFixed(4).toString();
             });
-            var lowest_price_info = newArray.find(function (obj){
+            var lowest_price_info = newArray.find(function (obj) {
                 return obj['3. low'] === stock_crypto_lowest.toFixed(4).toString();
             });
-           console.log(highest_price_info);
-           console.log(lowest_price_info);
-                debugger;
+            console.log(highest_price_info);
+            console.log(lowest_price_info);
+
             var chart = new CanvasJS.Chart("chartContainer", {
                 title: {
                     text: 'AAPL \n' + ' Last Refreshed:  ' + stock_crypto_info['3. Last Refreshed'],
@@ -137,35 +146,42 @@ function processURL(queryURL) {
                         // Change type to "doughnut", "line", "splineArea", etc.
                         type: "splineArea",
                         dataPoints: [
-                            { label:'Highest Price Date: ' + highest_price_info.date, y: stock_crypto_highest_price }, //this will be in what ever interval we get
-                            
-                         { label: "Lowest Price Date: " + lowest_price_info.date, y: stock_crypto_lowest},
+                            { label: 'Highest Price Date: ' + highest_price_info.date, y: stock_crypto_highest_price }, //this will be in what ever interval we get
+
+                            { label: "Lowest Price Date: " + lowest_price_info.date, y: stock_crypto_lowest },
 
                             { label: "Current Price Date: " + stock_crypto_info['3. Last Refreshed'], y: Number(newArray[0]['2. high']) }
                         ]
                     }
                 ]
-            });
-
-            var chart2 = new CanvasJS.Chart("chartContainer2",{
-                title :{
-                text: "Live Data"
-                },
-                data: [{
-                type: "pie",
-                dataPoints : [
-                    { label: 'Highest Price Date: ' + highest_price_info.date, y: stock_crypto_highest_price }, //this will be in what ever interval we get
-
-                            { label: "Current Price Date: " + stock_crypto_info['3. Last Refreshed'], y: Number(newArray[0]['2. high']) }
-                        ]
-                    }
-                ]
-                
-            });
-
-
+            });            
             chart.render();
-            chart2.render();
+
+                if (choseCrypto === true) {
+                    choseCrypto = false;
+                    var chart2 = new CanvasJS.Chart("chartContainer2", {
+                        title: {
+                            text: "Live Data"
+                        },
+                        data: [{
+                            type: "pie",
+                            dataPoints: [
+                                { label: 'Highest Price Date: ' + highest_price_info.date, y: stock_crypto_highest_price }, //this will be in what ever interval we get
+        
+                                { label: "Current Price Date: " + stock_crypto_info['3. Last Refreshed'], y: Number(newArray[0]['2. high']) }
+                            ]
+                        }
+                        ]
+        
+                    });
+        
+                    chart2.render();
+                }
+           
+
+        
+
+            
             //this save the price informations of the first date chosen. 
             var showInfo_date1 = newArray.find(function (obj) {
                 return obj.date === stock_chosen_date_1
@@ -193,7 +209,7 @@ function processURL(queryURL) {
             console.log(calculateInvestment(money_invested));
 
 
-            
+
 
 
         }); //end of ajax(then) call
