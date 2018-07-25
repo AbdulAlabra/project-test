6d2e73fb4564e7aa4719d004be5dec2ae83ee26c
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-07-25 10:50:36 -0700

    this code is working for both stock/crupto

--- google-api.js
+++ google-api.js
@@ -1,200 +1,42 @@
-var crypto_name = "BTC";
-var stock_name = "";
+var stock_chosen_name;
+var crypto_chosen_name;
+var stock_chosen_date_1;
+var stock_chosen_date_2;
+var money_invested;
 
-var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=AAPL&outputsize=full&apikey=Z192UWPZED4LZB5Z";
-var api_key = "Z192UWPZED4LZB5Z";
+$('button').on('click', function () {
+    stock_chosen_name = $('.stock-search').val();
+    crypto_chosen_name = $('.crypto-search').val();
+    stock_chosen_date_1 = $('.date1').val();
+    stock_chosen_date_2 = $('.date2').val();
+    money_invested = $('.investment').val();
 
-console.log(queryURL);
-// var stock = $("stock-box").val();
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    .then(function (response) {
-        var AA_from_2000_to_2018_stock = response;
-        var AA_convert_object_to_array = Object.values(AA_from_2000_to_2018_stock);
-        var AA_array_for_numbers = Object.values(AA_convert_object_to_array[1]);
-        var AA_array_for_dates = Object.keys(AA_convert_object_to_array[1]);
-console.log(response);
-console.log(AA_convert_object_to_array);
-console.log(AA_array_for_numbers);
-        //empty arrays to use later in the foor loop in order to seaprete high and low value.  
-        var array_for_low_values = [];
-        var array_for_high_values = [];
-        // console.log(AA_array_for_dates);
-        // console.log(AA_array_for_numbers);
+    var queryURL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + stock_chosen_name + "&outputsize=full&apikey=Z192UWPZED4LZB5Z";
 
-        var days_array_2018 = [];
-        var days_array_2017 = [];
-        var days_array_2016 = [];
-        var days_array_2015 = [];
-        var days_array_2014 = [];
-        var days_array_2013 = [];
-        var days_array_2012 = [];
-        var days_array_2011 = [];
-        var days_array_2010 = [];
-        var days_array_2009 = [];
-        var days_array_2008 = [];
-        var days_array_2007 = [];
-        var days_array_2006 = [];
-        var days_array_2005 = [];
-        var days_array_2004 = [];
-        var days_array_2003 = [];
-        var days_array_2002 = [];
-        var days_array_2001 = [];
-        var days_array_2000 = [];
+    var crypto_queryURL = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=" + crypto_chosen_name + "&market=CNY&apikey=Z192UWPZED4LZB5Z";
+    var api_key = "Z192UWPZED4LZB5Z";
+    console.log(stock_chosen_name.length);
 
+    if (stock_chosen_name.length >= 3) {
+        processURL(queryURL);
+        debugger;
+    }
+    else {
 
-        for (var i = 0; i < AA_array_for_numbers.length; i++) {
-            //every index has low and high value. so we should break every index into 2 parts; high and low.
+    }
 
-            // this capture the low value of the index and store it in the "low_values" variable.
-            var low_values = AA_array_for_numbers[i]['3. low'];
+    console.log(crypto_chosen_name.length);
+    if (crypto_chosen_name.length >= 3) {
 
-            //samething here but this will be for high values
-            var high_values = AA_array_for_numbers[i]['2. high'];
-            //gets the date of the index
-            var date_for_values = AA_array_for_dates[i];
+        processURL(crypto_queryURL);
+        debugger;
 
-            array_for_low_values.push(low_values);
-            array_for_high_values.push(high_values);
-            // array_for_high_values.push(high_values);
-        }
+    }
+    else {
 
-        // these variables find the lowest and highest values of AA_array_for_low_value and AA_array_for_high_value arrays. 
-        var lowVal_index = array_for_low_values.indexOf(Math.min(...array_for_low_values).toFixed(4));
-        var highVal_index = array_for_high_values.indexOf(Math.max(...array_for_high_values).toFixed(4));
+    }
 
-        for (var i = 0; i < AA_array_for_dates.length; i++) {
-
-            if (AA_array_for_dates[i].includes('2018')) { 
-                days_array_2018.push(AA_array_for_dates[i]);      
-            }
-             else if(AA_array_for_dates[i].includes('2017')){
-                days_array_2017.push(AA_array_for_dates[i]);
-                
-            }
-
-            else if(AA_array_for_dates[i].includes('2016')) {
-                days_array_2016.push(AA_array_for_dates[i]);
-            }
-
-            else if(AA_array_for_dates[i].includes('2015')) {
-                days_array_2015.push(AA_array_for_dates[i]);
-            }
-
-            else if(AA_array_for_dates[i].includes('2014')) {
-                days_array_2014.push(AA_array_for_dates[i]);
-            }
-
-            else if(AA_array_for_dates[i].includes('2013')) {
-                days_array_2013.push(AA_array_for_dates[i]);
-            }
-
-            else if(AA_array_for_dates[i].includes('2012')) {
-                days_array_2012.push(AA_array_for_dates[i]);
-            }
-
-            else if(AA_array_for_dates[i].includes('2011')) {
-                days_array_2011.push(AA_array_for_dates[i]);
-            }
-
-            else if(AA_array_for_dates[i].includes('2010')) {
-                days_array_2010.push(AA_array_for_dates[i]);
-            }
-
-            else if(AA_array_for_dates[i].includes('2009')) {
-                days_array_2009.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2008')) {
-                days_array_2008.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2007')) {
-                days_array_2007.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2006')) {
-                days_array_2006.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2005')) {
-                days_array_2005.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2004')) {
-                days_array_2004.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2003')) {
-                days_array_2003.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2002')) {
-                days_array_2002.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2001')) {
-                days_array_2001.push(AA_array_for_dates[i]);
-            }
-            else if(AA_array_for_dates[i].includes('2000')) {
-                days_array_2000.push(AA_array_for_dates[i]);
-            }
-            else{
-                break;
-
-            }
-
-        }
-        console.log(days_array_2018);
-        console.log(days_array_2017);
-        console.log(days_array_2016);
-        console.log(days_array_2015);
-        console.log(days_array_2014);
-        console.log(days_array_2013);
-        console.log(days_array_2012);
-        console.log(days_array_2011);
-        console.log(days_array_2010);
-        console.log(days_array_2009);
-        console.log(days_array_2008);
-        console.log(days_array_2007);
-        console.log(days_array_2006);
-        console.log(days_array_2005);
-        console.log(days_array_2004);
-        console.log(days_array_2003);
-        console.log(days_array_2002);
-        console.log(days_array_2001);
-        console.log(days_array_2000);
-
-
-        // console.log("----2018---");
-        // console.log(AA_array_for_dates[0]);
-        // console.log(AA_array_for_dates[138]);
-
-        // console.log("-----2017-----");
-        // console.log(AA_array_for_dates[389]);
-        // console.log(AA_array_for_dates[139]);
-
-        // console.log("-----2016-----");
-        // console.log(AA_array_for_dates[390]);
-        // console.log(AA_array_for_dates[641]);
-
-        // console.log("-----2015-----");
-        // console.log(AA_array_for_dates[642]);
-        // console.log(AA_array_for_dates[893]);
-
-        // console.log("-----2014-----");
-        // console.log(AA_array_for_dates[894]);
-        // console.log(AA_array_for_dates[1145]);
-
-        // console.log("-----2013-----");
-        // console.log(AA_array_for_dates[1146]);
-        // console.log(AA_array_for_dates[1397]);
-
-        // console.log("-----2012-----");
-        // console.log(AA_array_for_dates[1398]);
-        // console.log(AA_array_for_dates[1647]);
-
-        // console.log("-----2011-----");
-        // console.log(AA_array_for_dates[1648]);
-        // console.log(AA_array_for_dates[1899]);
-
-        // console.log("-----2010-----");
-        // console.log(AA_array_for_dates[1900]);
-        // console.log(AA_array_for_dates[2150]);
+}); //end of the click button 
 
 
 
@@ -205,57 +47,141 @@ console.log(AA_array_for_numbers);
 
 
 
+function processURL(queryURL) {
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
 
 
 
+            var turn_obj_to_array = Object.values(response);
+            var stock_crypto_info = turn_obj_to_array[0];
+            var stock_crypto_price = Object.values(turn_obj_to_array[1]);
+            var stock_crypto_dates = Object.keys(turn_obj_to_array[1]);
+            //we are getting both the dates and prices 
 
+            var newArray = stock_crypto_dates.map(function (x) {
+                var date_index = stock_crypto_dates.indexOf(x);
+                var price = stock_crypto_price[date_index];
+                price.date = x
 
+                //this will change some of the key names for crypto objects so that the rest of the code can function in the same way as stock
+                if (price['2. high'] === undefined) {
+                    var high_crypto_val = price['2b. high (USD)'];
+                    var low_crypto_val = price['3b. low (USD)'];
+                    var open_crypto_val = price['1b. open (USD)'];
+                    var close_crypto_val = price['4a. close (CNY)'];
 
+                    price['2. high'] = price['2b. high (USD)']
+                    delete price['2b. high (USD)'];
+                    price['2. high'] = Number(high_crypto_val).toFixed(4);
 
+                    price['3. low'] = price['3b. low (USD)'];
+                    delete price['3b. low (USD)'];
+                    price['3. low'] = Number(low_crypto_val).toFixed(4);
 
+                    price['1. open'] = price['1b. open (USD)'];
+                    delete price['1b. open (USD)'];
+                    price['1. open'] = Number(open_crypto_val).toFixed(4);
 
+                    price['4. close'] = price['4a. close (CNY)'];
+                    delete price['4a. close (CNY)'];
+                    price['4. close'] = Number(close_crypto_val).toFixed(4);
 
-
-        var chart = new CanvasJS.Chart("chartContainer", {
-            title: {
-                text: "Apple lnc."
-            },
-            data: [
-                {
-                    // Change type to "doughnut", "line", "splineArea", etc.
-                    type: "splineArea",
-                    dataPoints: [
-                        { label: AA_array_for_dates[lowVal_index], y: Number(array_for_low_values[lowVal_index]) },
-                        { label: AA_array_for_dates[highVal_index], y: Number(array_for_high_values[highVal_index]) },
-                        { label: "banana", y: 25 },
-                        { label: "mango", y: 30 },
-                        { label: "grape", y: 28 }
-                    ]
+                    return price
                 }
-            ]
-        });
-        chart.render();
+                else {
+                    return price
+                }
+            }); // end of the new array
 
-    }); //end of ajax(then) call
 
-// window.onload = function () {
-//     var chart = new CanvasJS.Chart("chartContainer", {
-//         title: {
-//             text: "Apple lnc."
-//         },
-//         data: [
-//             {
-//                 // Change type to "doughnut", "line", "splineArea", etc.
-//                 type: "column",
-//                 dataPoints: [
-//                     { label: "apple", y: 10 },
-//                     { label: "orange", y: 15 },
-//                     { label: "banana", y: 25 },
-//                     { label: "mango", y: 30 },
-//                     { label: "grape", y: 28 }
-//                 ]
-//             }
-//         ]
-//     });
-//     chart.render();
-// }
+            //finds the highest price in the last years 20 the stock/crypto 
+            var stock_crypto_highest_price = newArray.reduce(function (x, obj) {
+
+                var y = obj['2. high'];
+                if (y > x) {
+                    return Number(y)
+                }
+                else {
+                    return Number(x)
+                }
+            }, 0);
+            console.log(stock_crypto_highest_price);
+
+
+            var highest_price_info = newArray.find(function (obj) {
+
+                return obj['2. high'] === stock_crypto_highest_price.toFixed(4).toString();
+            });
+
+
+            var stock_crypto_lowest = newArray.reduce(function (x, obj) {
+                var y = obj['3. low'];
+                if (y > x) {
+                    return Number(x)
+                }
+                else {
+                    return Number(y)
+                }
+            }, 99999);
+
+            var chart = new CanvasJS.Chart("chartContainer", {
+                title: {
+                    text: 'AAPL \n' + ' Last Refreshed:  ' + stock_crypto_info['3. Last Refreshed'],
+
+                },
+                data: [
+                    {
+                        // Change type to "doughnut", "line", "splineArea", etc.
+                        type: "splineArea",
+                        dataPoints: [
+                            { label: 'Highest Price Date: ' + highest_price_info.date, y: stock_crypto_highest_price }, //this will be in what ever interval we get
+
+                            { label: "Current Price Date: " + stock_crypto_info['3. Last Refreshed'], y: Number(newArray[0]['2. high']) }
+                        ]
+                    }
+                ]
+            });
+
+            chart.render();
+
+            //if the date is not chosen, do nothing
+
+            //this save the price informations of the first date chosen. 
+            var showInfo_date1 = newArray.find(function (obj) {
+                return obj.date === stock_chosen_date_1
+            });
+            var showInfo_date2 = newArray.find(function (obj) {
+                return obj.date === stock_chosen_date_2
+            });
+
+
+            function calculateInvestment(x) {
+                var investment = x;
+                var date1_avg_price = (Number(showInfo_date1['2. high']) + Number(showInfo_date1['3. low'])) / 2; //from
+                var date2_avg_price = (Number(showInfo_date2['2. high']) + Number(showInfo_date2['3. low'])) / 2;  //to
+                var result = investment / date1_avg_price * date2_avg_price;
+                var percantageChange = (result - investment) / investment;
+                var gain_or_lose = result - investment;
+
+                return 'Your gain/lose: $' + gain_or_lose.toFixed(2) + '\n' + 'ROI: $' + result.toFixed(2) + '\n' + 'Percantage increase/deacrese: ' + percantageChange.toFixed(3) * 100 + '%'
+            }
+
+            console.log(showInfo_date1);
+            console.log(showInfo_date2);
+            console.log(calculateInvestment(money_invested));
+
+
+
+
+
+        }); //end of ajax(then) call
+} // end of process function
+
+
+
+
